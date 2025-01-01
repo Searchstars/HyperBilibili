0c8f81a31840b5156fc6c778830174d467894ed7
Searchstars <searchstars@163.com>
2025-01-01 14:21:16 +0800

    更新actions和构建脚本

--- scripts/build_s3.py
+++
@@ -1,8 +0,0 @@
-import os
-import buildtools
-
-DESIGN_WIDTH = 466
-
-buildtools.setManifestDesignWidth(DESIGN_WIDTH)
-
-os.system("aiot release --enable-custom-component --enable-jsc --enable-protobuf --enable-image-png8")
