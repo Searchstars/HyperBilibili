602fa0725da2118015b2d3b44f4d051452b184d4
Searchstars <searchstars@163.com>
2025-01-23 23:41:53 +0800

    动态详情初步实现

--- quickapp.config.js
+++ quickapp.config.js
@@ -18,17 +18,25 @@ const buildInfoPath = path.resolve(__dirname, 'src/buildinfo.ts');
 fs.writeFileSync(buildInfoPath, buildInfoContent, 'utf8');
 
 module.exports = {
+    // 在toolkit2.0以及更高版本，这实际上是对rspack的配置
     webpack: {
         module: {
             rules: [
                 {
-                    test: /\.tsx?$/,
-                    use: [
-                        {
-                            loader: "ts-loader",
+                    test: /\.ts$/,
+                    exclude: [/node_modules/],
+                    // 因为这实际上是针对rspack的配置，所以可以使用builtin:swc-loader
+                    // 使用该loader可以大幅度提升typescript转换效率
+                    loader: 'builtin:swc-loader',
+                    options: {
+                        jsc: {
+                            parser: {
+                                syntax: 'typescript',
+                            },
                         },
-                    ],
-                },
+                    },
+                    type: 'javascript/auto',
+                }
             ]
         },
         resolve: {
