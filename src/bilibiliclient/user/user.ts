852bc848a55c571a7ee99416382d14e40ff82634
Searchstars <searchstars@163.com>
2025-01-30 16:25:56 +0800

    用户主页初版

--- src/bilibiliclient/user/user.ts
+++ src/bilibiliclient/user/user.ts
@@ -17,6 +17,34 @@ export const BilibiliClientUserMethods = {
         return response.data.data
     },
 
+    // 获取单个用户投稿的视频的代表作
+    async getUserMasterPieceByUID(this: any, uid: String) {
+        const url = `https://api.bilibili.com/x/space/masterpiece`;
+        const response = await this.getRequest(`${url}?vmid=${uid}`);
+
+        return response.data.data
+    },
+
+    // 获取用户投稿列表（分页）
+    async getUserVideosByUID(this: any, uid: String, pn: Number, ps: Number = 5) {
+        const url = `https://api.bilibili.com/x/space/wbi/arc/search`;
+        const response = await this.getRequestWbi(url, {
+            mid: uid,
+            pn,
+            ps
+        });
+
+        return response.data.data
+    },
+
+    // 获取用户空间动态列表
+    async getUserDynamicListByUID(this: any, uid: String) {
+        const url = `https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/space`;
+        const response = await this.getRequest(`${url}?host_mid=${uid}`);
+
+        return response.data.data
+    },
+
     // 获取单个用户的导航栏状态数
     async getUserNavnumByUID(this: any, uid: String) {
         const url = `https://api.bilibili.com/x/space/navnum`;
