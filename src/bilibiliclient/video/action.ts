9e310803d50ce8b54065223414e8800cbb76a8f7
hanyixuanten <hanyixuanten@outlook.com>
2026-07-03 10:49:51 +0800

    添加收藏夹选择功能

--- src/bilibiliclient/video/action.ts
+++ src/bilibiliclient/video/action.ts
@@ -62,4 +62,32 @@ export const BilibiliClientVideoActionMethods = {
             return false;
         }
     },
+
+    // 收藏视频至指定收藏夹（支持多个）
+    async starVideoToFavFolders(this: any, aid: string, folderIds: string[]): Promise<any> {
+        try {
+            const add_media_ids = folderIds.join(',');
+            const url = `https://api.bilibili.com/x/v3/fav/resource/deal`;
+            const data = `rid=${aid}&csrf=${this.biliJct}&type=2&add_media_ids=${add_media_ids}&del_media_ids=`;
+            const response = await this.postRequest(url, data, "application/x-www-form-urlencoded");
+            return response.data.code;
+        } catch (error) {
+            global.logger.error("Error starring video to folders: ", error);
+            return false;
+        }
+    },
+
+    // 从指定收藏夹取消收藏视频（支持多个）
+    async unstarVideoFromFavFolders(this: any, aid: string, folderIds: string[]): Promise<any> {
+        try {
+            const del_media_ids = folderIds.join(',');
+            const url = `https://api.bilibili.com/x/v3/fav/resource/deal`;
+            const data = `rid=${aid}&csrf=${this.biliJct}&type=2&add_media_ids=&del_media_ids=${del_media_ids}`;
+            const response = await this.postRequest(url, data, "application/x-www-form-urlencoded");
+            return response.data.code;
+        } catch (error) {
+            global.logger.error("Error unstarring video from folders: ", error);
+            return false;
+        }
+    },
 }
